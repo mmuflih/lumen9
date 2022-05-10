41585e09e0031adf88ea57d3e671095fdf1669d6
Muflih Kholidin <muflic.24@gmail.com>
2022-05-11 06:47:46 +0700

    Added commands
    - route
    - make model

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,8 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        Commands\MakeModelCommand::class,
+        Commands\RouteCommand::class,
     ];
 
     /**
